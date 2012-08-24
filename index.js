2af033767f1cc756786d06e3b27775b37fb5ba93
Luc <luc.tribolet@gmail.com>
2012-08-24 13:10:48 +0100

    Changes needed for full websocket support

--- index.js
+++ index.js
@@ -1,1 +1,257 @@
-module.exports = require('./lib/webrtc.io');
+//SERVER
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+var WebSocketServer = require('websocket').server;
+
+var iolog = function(msg){console.log(msg);};
+
+for(var i=0; i < process.argv.length; i++){
+  var arg = process.argv[i];
+  if(arg === "-debug"){
+    iolog = function(msg){console.log(msg);};
+    console.log('Debug mode on!');
+  }
+}
+
+
+// Used for callback publish and subscribe
+if (typeof rtc === "undefined") {
+  var rtc = {};
+}
+//Array to store connections
+rtc.sockets = [];
+
+rtc.rooms = [];
+
+// Holds callbacks for certain events.
+rtc._events = {};
+
+rtc.on = function(eventName, callback) {
+  rtc._events[eventName] = rtc._events[eventName] || [];
+  rtc._events[eventName].push(callback);
+};
+
+rtc.fire = function(eventName, _) {
+  var events = rtc._events[eventName];
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  if (!events) {
+    return;
+  }
+
+  for (var i = 0, len = events.length; i < len; i++) {
+    events[i].apply(null, args);
+  }
+};
+
+var manager = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+manager.rtc = rtc;
+attachEvents(manager);
+
+function attachEvents(manager) {
+
+  manager.on('request', function(socket) {
+	var connection = socket.accept(null, socket.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    iolog('connect');
+
+    socket.id = id();
+    iolog('new socket got id: ' + socket.id);
+
+    rtc.sockets.push(socket);
+
+    socket.on('message', function(data) {
+      var json = JSON.parse(data);
+      rtc.fire(json.eventName, json, socket);
+    });
+
+    socket.on('close', function() {
+      iolog('close');
+
+      // find socket to remove
+      var i = rtc.sockets.indexOf(socket);
+      // remove socket
+      rtc.sockets.splice(i, 1);
+
+      // remove from rooms
+      for(var room in rtc.rooms) {
+        var exist = room.indexOf(socket.id);
+        if(exist !== -1){
+          rtc.rooms.splice(rtc.rooms.indexOf(room), 1);
+        }
+      }
+
+      // remove the disconnected socket from the sockets array and send out
+      // a notification to all connected peers that socket was removed.
+      for (var j = 0; j < rtc.sockets.length; j++) {
+        var id = rtc.sockets[j].id;
+        if (id === socket.id) {
+          rtc.sockets.splice(j, 1);
+          j--;
+        } else {
+          rtc.sockets[j].send(JSON.stringify({
+            "eventName": "remove_peer_connected",
+            "socketId": socket.id
+          }), function(error) {
+            if (error) {
+              console.log(error);
+            }
+          });
+        }
+      }
+
+      // call the disconnect callback
+      rtc.fire('disconnect', rtc);
+
+    });
+
+    // call the connect callback
+    rtc.fire('connect', rtc);
+
+  });
+
+  // manages the built-in room functionality
+  rtc.on('join_room', function(data, socket) {
+    iolog('join_room');
+
+    var connectionsId = [];
+    var roomList = rtc.rooms[data.room] || [];
+
+    roomList.push(socket.id);
+    rtc.rooms[data.room] = roomList;
+    
+
+    for (var i = 0; i < roomList.length; i++) {
+      var id = roomList[i];
+
+      if (id == socket.id) {
+        continue;
+      } else {
+
+        connectionsId.push(id);
+        var soc = rtc.getSocket(data.room, id);
+
+        // inform the peers that they have a new peer
+        if (soc) {
+          soc.send(JSON.stringify({
+            "eventName": "new_peer_connected",
+            "socketId": socket.id
+          }), function(error) {
+            if (error) {
+              console.log(error);
+            }
+          });
+        }
+      }
+    }
+    // send new peer a list of all prior peers
+    socket.send(JSON.stringify({
+      "eventName": "get_peers",
+      "connections": connectionsId
+    }), function(error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  });
+
+  //Receive ICE candidates and send to the correct socket
+  rtc.on('send_ice_candidate', function(data, socket) {
+    iolog('send_ice_candidate');
+    var soc = rtc.getSocket(data.room, data.socketId);
+
+    if (soc) {
+      soc.send(JSON.stringify({
+        "eventName": "receive_ice_candidate",
+        "label": data.label,
+        "candidate": data.candidate,
+        "socketId": socket.id
+      }), function(error) {
+        if (error) {
+          console.log(error);
+        }
+      });
+
+      // call the 'recieve ICE candidate' callback
+      rtc.fire('receive ice candidate', rtc);
+    }
+  });
+
+  //Receive offer and send to correct socket
+  rtc.on('send_offer', function(data, socket) {
+    iolog('send_offer');
+    var soc = rtc.getSocket(data.room, data.socketId);
+
+    if (soc) {
+      soc.send(JSON.stringify({
+        "eventName": "receive_offer",
+        "sdp": data.sdp,
+        "socketId": socket.id
+      }), function(error) {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
+    // call the 'send offer' callback
+    rtc.fire('send offer', rtc);
+  });
+
+  //Receive answer and send to correct socket
+  rtc.on('send_answer', function(data, socket) {
+    iolog('send_answer');
+    var soc = rtc.getSocket(data.room, data.socketId);
+
+    if (soc) {
+      soc.send(JSON.stringify({
+        "eventName": "receive_answer",
+        "sdp": data.sdp,
+        "socketId": socket.id
+      }), function(error) {
+        if (error) {
+          console.log(error);
+        }
+      });
+      rtc.fire('send answer', rtc);
+    }
+  });
+}
+
+// generate a 4 digit hex code randomly
+function S4() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+// make a REALLY COMPLICATED AND RANDOM id, kudos to dennis
+function id() {
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+rtc.getSocket = function(room, id) {
+  var connections = rtc.sockets;
+  if (!connections) {
+    // TODO: Or error, or customize
+    return;
+  }
+
+  for (var i = 0; i < connections.length; i++) {
+    var socket = connections[i];
+    if (id === socket.id) {
+      return socket;
+    }
+  }
+}
